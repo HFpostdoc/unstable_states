6bdcea84506a262098236ee31d36a237556a16ac
mkl <mkl48@nau.edu>
2014-05-22 13:57:21 -0400

    update functions

--- src/unstable_states.R
+++ src/unstable_states.R
@@ -2,26 +2,12 @@
 ###Idea = AMEllison
 ###Code = MKLau
 
-###Use Bob May's logistic growth parameterization for ecosystems
-###Use the quadratic recurrence equation (or quadratic map) which May develops by defining X = bN/a
-##Use the r notation that is more common now instead of a
-
+##Using Bob May's logistic growth parameterization for ecosystems
+##Using the quadratic recurrence equation (or quadratic map) which May develops by defining X = bN/a
+##Here, we use the r notation that is more common now instead of a
 ##From dN/dt = rN(K-N) / K, setting x = N/K yields
 ##dx/dt = rx(1-x) and
 ##xt+1 = rxt (1-xt)
-
-plot.phase <- function(x,col=1){
-  plot(x[2:nrow(x),2]~x[1:(nrow(x)-1),2],col=col)
-}
-
-lines.phase <- function(x,col=1){
-  lines(spline(x[2:nrow(x),2]~x[1:(nrow(x)-1),2]),col=col)
-}
-
-points.phase <- function(x,col=1){
-  points(x[2:nrow(x),2]~x[1:(nrow(x)-1),2],col=col)
-}
-
 
 qre <- function(x0=0.1,r=2,tf=500,ts=1){
   t <- seq(0,tf,by=ts)
@@ -31,3 +17,19 @@ qre <- function(x0=0.1,r=2,tf=500,ts=1){
   out <- cbind(Time=t,X=out)
   out
 }
+
+###plots the phase space xt+1 ~ xt
+plot.phase <- function(x,col=1){
+  plot(x[2:nrow(x),2]~x[1:(nrow(x)-1),2],col=col)
+}
+
+###plots lines for the phase space using spline
+lines.phase <- function(x,col=1){
+  lines(spline(x[2:nrow(x),2]~x[1:(nrow(x)-1),2]),col=col)
+}
+
+###plots points for the phase space
+points.phase <- function(x,col=1){
+  points(x[2:nrow(x),2]~x[1:(nrow(x)-1),2],col=col)
+}
+
